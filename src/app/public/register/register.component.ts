f71ad495048197fbda1f7b47dfb46a5e3d285e01
Roger <rogernuica3@gmail.com>
2021-04-10 11:21:31 +0300

    register component

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css', './../public.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        password_confirm: ''
+      }
+    );
   }
 
+  submit(): void {
+    const data = this.form.getRawValue();
+
+    this.authService.login(data).subscribe(
+       res => {
+        console.log(res);
+      }
+    );
+  }
 }
